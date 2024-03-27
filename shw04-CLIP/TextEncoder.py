ef7f20a98932120174c884debb86eb2bd1bd09f2
Polina Kadeyshvili <pshvili2002@gmail.com>
2024-03-27 19:01:32 +0300

    rest of assignments added

---
+++ shw04-CLIP/TextEncoder.py
@@ -0,0 +1,28 @@
+from transformers import DistilBertModel, DistilBertConfig
+import torch.nn as nn
+
+class TextEncoder(nn.Module):
+    def __init__(self, model_name="distilbert-base-uncased", pretrained=True, trainable=False):
+        super().__init__()
+        """
+        Create the model and set its weights frozen. 
+        Use Transformers library docs to find out how to do this.
+        """
+        if pretrained:
+            self.model = DistilBertModel.from_pretrained(model_name)
+        else:
+            self.model = DistilBertModel(config=DistilBertConfig())
+            
+        for p in self.model.parameters():
+            p.requires_grad = trainable
+        self.target_token_idx = 0
+
+    def forward(self, input_ids, attention_mask):
+        """
+        Pass the arguments through the model and make sure to return CLS token embedding
+        """
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask)
+        last_hidden_state = output.last_hidden_state
+        return last_hidden_state[:, self.target_token_idx, :]
+
+
